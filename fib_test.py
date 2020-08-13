22b49383bbca03ebc5a7568749db233169b56182
Yun0113 <69342165+Yun0113@users.noreply.github.com>
2020-08-13 14:50:46 +0800

    Add files via upload

---
+++ fib_test.py
@@ -0,0 +1,27 @@
+import unittest
+import sys
+sys.path.append(r"C:\Users\Administrator\Documents")
+from fib import fib
+
+class FibTest(unittest.TestCase):
+    def test_generate_5_fibs(self):
+        self.assertEqual(fib(5), [1, 1, 2, 3, 5])
+
+    def test_generate_10_fibs(self):
+        pass
+
+    def test_generate_negative_number_fibs(self):
+        pass
+
+    def test_generate_0_number_fibs(self):
+        pass
+
+    def test_generate_1_number_fibs(self):
+        pass
+
+    def test_generate_2_number_fibs(self):
+        pass
+
+
+if __name__ == '__main__':
+    unittest.main()
